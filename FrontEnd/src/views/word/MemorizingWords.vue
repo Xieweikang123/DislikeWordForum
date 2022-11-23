29c7b6bb4632957214440be8c17423a8e5007e2c
walker <2303061691@qq.com>
2022-11-23 20:54:38 +0800

    单词朗读，配置声音

--- FrontEnd/src/views/word/MemorizingWords.vue
+++ FrontEnd/src/views/word/MemorizingWords.vue
@@ -1,41 +1,53 @@
 <template>
-  <div class="margin60Auto memContainer">
-    单词记录数 ≥
-    <el-input-number
-      v-model="recordTimes"
-      @change="handleChange"
-      :min="1"
-      label="描述文字"
-    ></el-input-number>
+  <div>
+    <div class="rightStyle">
+      <el-switch v-model="configEntity.isOpenVoice" active-text="开启朗读声音">
+      </el-switch>
+    </div>
+    <div class="margin60Auto memContainer">
+      单词记录数 ≥
+      <el-input-number
+        v-model="recordTimes"
+        @change="handleChange"
+        :min="1"
+        label="描述文字"
+      ></el-input-number>
 
-    共 {{ allData.length }}个
+      共 {{ allData.length }}个
 
-    <div style="margin: 16px 0px">
-      记录数:
-      <el-button
-        type="primary"
-        @click="changeRecordTimes(-1)"
-        @keyup.space.native.prevent="fun"
-        plain
-        >-</el-button
+      <div style="margin: 16px 0px">
+        记录数:
+        <el-button
+          type="primary"
+          @click="changeRecordTimes(-1)"
+          @keyup.space.native.prevent="fun"
+          plain
+          >-</el-button
+        >
+        <el-button
+          type="primary"
+          @click="changeRecordTimes(1)"
+          @keyup.space.native.prevent="fun"
+          plain
+          >+</el-button
+        >
+      </div>
+      <div class="wordBall" @click="wordClick()">
+        {{ currentItem.word }}
+        <audio
+          controls
+          ref="audio"
+          :src="audioSrc"
+          style="display: none"
+        ></audio>
+      </div>
+      <div v-show="isShowTranslate">
+        {{ currentItem.translate }}
+      </div>
+      <el-button style="margin: 19px 61px" @click="setRandomOne" type="primary"
+        >换一个</el-button
       >
-      <el-button
-        type="primary"
-        @click="changeRecordTimes(1)"
-        @keyup.space.native.prevent="fun"
-        plain
-        >+</el-button
-      >
     </div>
-    <div class="wordBall" @click="isShowTranslate = !isShowTranslate">
-      {{ currentItem.word }}
-    </div>
-    <div v-show="isShowTranslate">
-      {{ currentItem.translate }}
-    </div>
-    <el-button style="margin: 19px 61px" @click="setRandomOne" type="primary"
-      >换一个</el-button
-    >
   </div>
 </template>
 
@@ -44,6 +56,10 @@ export default {
   components: {},
   data() {
     return {
+      configEntity: {
+        isOpenVoice: false,
+      },
+      audioSrc: "https://dict.youdao.com/dictvoice?audio=individual&type=2",
       isShowTranslate: false,
       allData: [],
       currentItem: {},
@@ -51,12 +67,50 @@ export default {
     };
   },
   watch: {
+    //配置改变，储存
+    configEntity: {
+      handler(nVal) {
+        console.log("watch configEntity");
+        localStorage.setItem("configEntity", JSON.stringify(nVal));
+      },
+      deep: true,
+    },
+    "currentItem.word": {
+      handler(nVal) {
+        var that = this;
+        //开启朗读
+        if (that.configEntity.isOpenVoice) {
+          this.$refs.audio.load();
+          this.audioSrc = `https://dict.youdao.com/dictvoice?audio=${this.currentItem.word}&type=2`;
+          this.$refs.audio.addEventListener("canplay", function (res) {
+            console.log("can play", res);
+            that.$refs.audio.play();
+          });
+        }
+        //关闭翻译
+        this.isShowTranslate = false;
+        //两秒之后，点击
+        setTimeout(() => {
+          that.wordClick();
+        }, 2500);
+        // this.$refs.audio.play();
+      },
+    },
     allData() {
       this.setRandomOne();
     },
   },
   mounted() {
     var that = this;
+
+    //  localStorage.setItem("configEntity", JSON.stringify(nVal));
+    //储存配置
+    var configEntityStorage = localStorage.getItem("configEntity");
+    console.log("configEntityStorage", configEntityStorage);
+    if (configEntityStorage) {
+      that.configEntity = JSON.parse(configEntityStorage);
+    }
+
     this.recordTimes = window.localStorage.getItem("recordTime");
     if (!this.recordTimes) {
       this.recordTimes = 2;
@@ -75,6 +129,53 @@ export default {
     };
   },
   methods: {
+    // //请求并播放音频
+    // playAudio() {
+    //   var that = this;
+    //   that.$http
+    //     .get(
+    //       `https://dict.youdao.com/dictvoice?audio=${this.currentItem.word}&type=2`,
+    //       {
+    //         headers: {
+    //           "Access-Control-Allow-Origin": "*",
+    //         },
+    //       }
+    //     )
+    //     .then((response) => response.blob())
+    //     .then((blob) => {
+    //       this.$refs.audio.srcObject = blob;
+    //       this.$refs.audio.play();
+    //     });
+    // },
+    playAudio() {},
+    //点击单词
+    wordClick() {
+      console.log("click");
+      this.isShowTranslate = !this.isShowTranslate;
+      // // this.audioSrc = `https://dict.youdao.com/dictvoice?audio=${this.currentItem.word}&type=2`;
+      // // this.$refs.audio.load();
+      //开启朗读
+      if (this.configEntity.isOpenVoice) {
+        this.$refs.audio.play();
+      }
+
+      // var playPromise = this.$refs.audio.play();
+      // if (playPromise !== undefined) {
+      //   playPromise
+      //     .then((_) => {
+      //       // Automatic playback started!
+      //       // Show playing UI.
+      //       // We can now safely pause video...
+      //       // video.pause();
+      //       this.$refs.audio.play();
+      //     })
+      //     .catch((error) => {
+      //       console.log("play error", error);
+      //       // Auto-play was prevented
+      //       // Show paused UI.
+      //     });
+      // }
+    },
     fun() {
       console.log("空格");
     },
@@ -126,6 +227,10 @@ export default {
 };
 </script>
 <style scoped>
+.rightStyle {
+  position: absolute;
+  right: 7%;
+}
 .memContainer {
   position: relative;
   /* left: -12%; */
