ab011d3308f1e99c8b05637986d0e10daef6df62
walker <2303061691@qq.com>
2023-01-29 22:19:12 +0800

    简单的网站服务器

--- BackendAPI/TestConsole/Program.cs
+++ BackendAPI/TestConsole/Program.cs
@@ -1,176 +1,36 @@
-﻿using System.Linq.Expressions;
+﻿using Microsoft.AspNetCore.Builder;
+using System.Linq.Expressions;
 using System.Net;
 using System.Speech;
 using System.Speech.Synthesis;
 
 
-// The Builder interface specifies methods for creating the different parts
-// of the Product objects.
-public interface IBuilder
-{
-
-    /// <summary>
-    /// 薯条
-    /// </summary>
-    void BuildPartA();
-    /// <summary>
-    /// 可乐
-    /// </summary>
-    void BuildPartB();
-    /// <summary>
-    /// 汉堡
-    /// </summary>
-    void BuildPartC();
-}
-
-
-
-// The Concrete Builder classes follow the Builder interface and provide
-// specific implementations of the building steps. Your program may have
-// several variations of Builders, implemented differently.
-public class ConcreteBuilder : IBuilder
-{
-    private Product _product = new Product();
-
-    // A fresh builder instance should contain a blank product object, which
-    // is used in further assembly.
-    public ConcreteBuilder()
-    {
-        this.Reset();
-    }
-
-    public void Reset()
-    {
-        this._product = new Product();
-    }
-
-    // All production steps work with the same product instance.
-    public void BuildPartA()
-    {
-        this._product.Add("薯条");
-    }
-
-    public void BuildPartB()
-    {
-        this._product.Add("可乐");
-    }
-
-    public void BuildPartC()
-    {
-        this._product.Add("汉堡");
-    }
-
-    // Concrete Builders are supposed to provide their own methods for
-    // retrieving results. That's because various types of builders may
-    // create entirely different products that don't follow the same
-    // interface. Therefore, such methods cannot be declared in the base
-    // Builder interface (at least in a statically typed programming
-    // language).
-    //
-    // Usually, after returning the end result to the client, a builder
-    // instance is expected to be ready to start producing another product.
-    // That's why it's a usual practice to call the reset method at the end
-    // of the `GetProduct` method body. However, this behavior is not
-    // mandatory, and you can make your builders wait for an explicit reset
-    // call from the client code before disposing of the previous result.
-    public Product GetProduct()
-    {
-        Product result = this._product;
-
-        this.Reset();
-
-        return result;
-    }
-}
-
-// It makes sense to use the Builder pattern only when your products are
-// quite complex and require extensive configuration.
-//
-// Unlike in other creational patterns, different concrete builders can
-// produce unrelated products. In other words, results of various builders
-// may not always follow the same interface.
-public class Product
-{
-    private List<object> _parts = new List<object>();
-
-    public void Add(string part)
-    {
-        this._parts.Add(part);
-    }
-
-    public string ListParts()
-    {
-        string str = string.Empty;
-
-        for (int i = 0; i < this._parts.Count; i++)
-        {
-            str += this._parts[i] + ", ";
-        }
-
-        str = str.Remove(str.Length - 2); // removing last ",c"
-
-        return "已点食物: " + str + "\n";
-    }
-}
-
-// The Director is only responsible for executing the building steps in a
-// particular sequence. It is helpful when producing products according to a
-// specific order or configuration. Strictly speaking, the Director class is
-// optional, since the client can control builders directly.
-public class Director
-{
-    private IBuilder _builder;
-
-    public IBuilder Builder
-    {
-        set { _builder = value; }
-    }
-
-    // The Director can construct several product variations using the same
-    // building steps.
-    //汉堡可乐套餐
-    public void BuildMinimalViableProduct()
-    {
-        this._builder.BuildPartB();
-        this._builder.BuildPartC();
-    }
-
-    public void BuildFullFeaturedProduct()
-    {
-        this._builder.BuildPartA();
-        this._builder.BuildPartB();
-        this._builder.BuildPartC();
-    }
-}
 public class Program
 {
 
 
 
-    public static void Main()
+    public static void Main(string[] args)
     {
 
-        // The client code creates a builder object, passes it to the
-        // director and then initiates the construction process. The end
-        // result is retrieved from the builder object.
-        var director = new Director();
-        var builder = new ConcreteBuilder();
-        director.Builder = builder;
 
-        Console.WriteLine("Standard basic product:");
-        director.BuildMinimalViableProduct();
-        Console.WriteLine(builder.GetProduct().ListParts());
 
-        Console.WriteLine("Standard full featured product:");
-        director.BuildFullFeaturedProduct();
-        Console.WriteLine(builder.GetProduct().ListParts());
+        //Console.WriteLine();
+        ////  Invoke this sample with an arbitrary set of command line arguments.
+        //string[] arguments = Environment.GetCommandLineArgs();
 
-        // Remember, the Builder pattern can be used without a Director
-        // class.
-        Console.WriteLine("Custom product:");
-        builder.BuildPartA();
-        builder.BuildPartC();
-        Console.Write(builder.GetProduct().ListParts());
+        //Console.WriteLine("GetCommandLineArgs: {0}", string.Join(", ", arguments));
+
+
+        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+        WebApplication app = builder.Build();
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        // Add a single endpoint
+        app.MapGet("/", () => "Hello World!");
+        app.Run();
 
 
         Console.WriteLine("ok");
